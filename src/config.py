e484453a070cfdf6c7f5d41cf8d224b24fa89842
Chetana Krishnan <63806095+chetana348@users.noreply.github.com>
2025-07-09 18:07:43 -0500

    update src utils

---
+++ src/config.py
@@ -0,0 +1,10 @@
+Config = {
+
+    'data_dir': r'D:\PhD\Prostate\Data\samunet\x',
+    'random_seed': 000000,
+    'sam_checkpoint': r"C:\Users\UAB\CK_WorkPlace\PhD\Prostate\samunet\sam_vit_h_4b8939.pth",
+    'img_size': 256,
+    'norm_mean': [123.675, 116.28, 103.53],
+    'norm_std': [58.395, 57.12, 57.375],
+    'out_dir': r"C:\Users\UAB\CK_WorkPlace\PhD\Prostate\samunet\weights\proposed\tl\x_on_uabnn\resnet",
+}
